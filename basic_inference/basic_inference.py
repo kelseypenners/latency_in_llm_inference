b1fedc9a613207a98e0015a22fa69fe87191f51e
kpenners <k.penners@student.tue.nl>
2026-02-18 11:20:26 +0100

    organization

---
+++ basic_inference/basic_inference.py
@@ -0,0 +1,21 @@
+import torch
+from vllm import LLM, SamplingParams
+
+model_id = "../Llama-3.2-1B"
+llm = LLM(model_id)
+
+# model prompts
+prompts = [
+    "Hello, my name is",
+    "San Francisco is a",
+    "The capital of France is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"prompt: {prompt!r}, generated text: {generated_text!r}")
