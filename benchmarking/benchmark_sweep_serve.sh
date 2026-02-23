3effb2b33c9910d3197ee3ee11faa0f1c8183631
kpenners <k.penners@student.tue.nl>
2026-02-23 09:35:15 +0100

    update benchmark script

--- benchmarking/benchmark_sweep_serve.sh
+++ benchmarking/benchmark_sweep_serve.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# server should already be running on port 8000!
-# CUDA_VISIBLE_DEVICES=3 vllm serve "./Llama-3.2-1B" --port 8000
 
-MODEL="./Llama-3.2-1B"
+MODEL="meta-llama/Llama-3.2-8B"
 GPU_ID=3
 GPU_TYPE="v100"
-DATE="19-02-2026"
+DATE="23-02-2026"
 OUTDIR="../results/single-gpu/${DATE}/${GPU_TYPE}/"
 
 mkdir -p $OUTDIR
@@ -15,9 +13,9 @@ echo "starting benchmark sweep"
 echo ""
 
 CUDA_VISIBLE_DEVICES=$GPU_ID vllm bench sweep serve \
-    --serve-cmd "vllm serve ${MODEL} -- port 8000" \
-    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --num-prompts 100" \
-    --num-runs 5 \
+    --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.8" \
+    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --num-prompts 1000 --save-result -- num-warmups 20 --ignore-eos True --max-concurrency 1" \
+    --num-runs 10 \
     --bench-params "./bench_params.json" \
     --output-dir $OUTDIR
 
