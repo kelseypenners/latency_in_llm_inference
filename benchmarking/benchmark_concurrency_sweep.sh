01e40e33d96630f09fc0905e68246cbbcce5aa6a
kpenners <kpenners@shy-fec.ele.tue.nl>
2026-02-25 11:27:40 +0100

    create concurrency script

--- benchmarking/benchmark_concurrency_sweep.sh
+++ benchmarking/benchmark_concurrency_sweep.sh
@@ -3,7 +3,7 @@
 MODEL="../Llama-3.1-8B"
 GPU_ID=7
 GPU_TYPE="a100"
-DATE="23-02-2026"
+DATE="25-02-2026"
 OUTDIR="../results/single-gpu/${DATE}/${GPU_TYPE}/"
 
 mkdir -p $OUTDIR
@@ -13,9 +13,9 @@ echo ""
 
 CUDA_VISIBLE_DEVICES=$GPU_ID vllm bench sweep serve \
     --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.85" \
-    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --num-prompts 100 --save-result --num-warmups 20 --ignore-eos --max-concurrency 1" \
+    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --num-prompts 100 --save-result --num-warmups 20 --ignore-eos" \
     --num-runs 3 \
-    --bench-params "./bench_params.json" \
+    --bench-params "./bench_concurrency_params.json" \
     --output-dir $OUTDIR
 
 echo "========================================"
