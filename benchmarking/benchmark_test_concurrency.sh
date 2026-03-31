d01e47343d8e508a291e4f642991c1fbc55a1c1b
kpenners <kpenners@shy-fec.ele.tue.nl>
2026-03-31 11:31:19 +0200

    benchmarking script work

--- benchmarking/benchmark_test_concurrency.sh
+++ benchmarking/benchmark_test_concurrency.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 MODEL="../Llama-3.1-8B"
-GPU_ID=6,7
+GPU_ID=7
 GPU_TYPE="a100"
 EXPERIMENT="concurrency-sweep"
-OUTDIR="/mnt/kpenners/results/multi-gpu/${EXPERIMENT}/${GPU_TYPE}/"
+OUTDIR="/mnt/kpenners/results/single-gpu/${EXPERIMENT}/${GPU_TYPE}/"
 
 mkdir -p $OUTDIR
 
@@ -12,13 +12,13 @@ echo "starting benchmark sweep"
 echo ""
 
 CUDA_VISIBLE_DEVICES=$GPU_ID vllm bench sweep serve \
-    --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.85 --max-num-seqs 1024 \
-    --tensor-parallel-size 2 --max-model-len 80000" \
+    --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.85 --max-num-seqs 1024 --max-model-len 80000" \
     --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --random-input-len 512 \
-    --random-output-len 512 --num-prompts 1000 --save-result --metric-percentiles 75,90,99 \
+    --random-output-len 512 --save-result --metric-percentiles 75,90,99 \
     --num-warmups 20 --ignore-eos --ready-check-timeout-sec 2000" \
-    --num-runs 2 \
-    --bench-params "./bench_concurrency_params.json" \
+    --num-runs 1 \
+    --bench-params "./bench_test_concurrency.json
+    " \
     --output-dir $OUTDIR\
 
 echo "========================================"
