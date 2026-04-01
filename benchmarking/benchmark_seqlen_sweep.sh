d0ea149b078d7e6f9f781e29e0b01132446ae5ff
kpenners <kpenners@funnotch.ele.tue.nl>
2026-04-01 10:27:33 +0200

    change seqlen params for extension

--- benchmarking/benchmark_seqlen_sweep.sh
+++ benchmarking/benchmark_seqlen_sweep.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-MODEL="../Llama-3.1-8B"
-GPU_ID=7
-GPU_TYPE="a100"
+MODEL="/mnt/kpenners/Llama-3.1-8B"
+GPU_ID=3
+GPU_TYPE="v100"
 EXPERIMENT="seqlen-sweep"
-OUTDIR="../results/single-gpu/${EXPERIMENT}/${GPU_TYPE}/"
+OUTDIR="/mnt/kpenners/results/single-gpu/${EXPERIMENT}/${GPU_TYPE}/"
 
 mkdir -p $OUTDIR
 
@@ -12,10 +12,11 @@ echo "starting benchmark sweep"
 echo ""
 
 CUDA_VISIBLE_DEVICES=$GPU_ID vllm bench sweep serve \
-    --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.85" \
-    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random --num-prompts 100 --save-result --num-warmups 20 --ignore-eos --max-concurrency 1" \
-    --num-runs 3 \
-    --bench-params "./bench_seqlens_params.json" \
+    --serve-cmd "vllm serve ${MODEL} --port 8000 --gpu-memory-utilization 0.85 --max-model-len 85000" \
+    --bench-cmd "vllm bench serve --backend openai --model ${MODEL} --dataset-name random \
+    --num-prompts 25 --save-result --num-warmups 20 --ignore-eos --max-concurrency 1" \
+    --num-runs 2 \
+    --bench-params "./bench_seqlen_params_extended.json" \
     --output-dir $OUTDIR
 
 echo "========================================"
