a9849b137024916b4792da8894d05dcb9496bf47
kpenners <kpenners@shy-fec.ele.tue.nl>
2026-04-08 11:48:52 +0200

    add labels to configs and script

--- benchmarking/run_experiment.sh
+++ benchmarking/run_experiment.sh
@@ -13,6 +13,7 @@ fi
 # read config file
 experiment=$(jq -r '.experiment' "$CONFIG")
 experiment_type=$(jq -r '.experiment_type' "$CONFIG")
+label=$(jq -r '.label // ""' "$CONFIG")
 model=$(jq -r '.model' "$CONFIG")
 gpu_ids=$(jq -r '.gpu_ids' "$CONFIG")
 gpu_type=$(jq -r '.gpu_type' "$CONFIG")
@@ -25,6 +26,7 @@ base_outdir=$(jq -r '.results' "$CONFIG")
 disable_p2p=$(jq -r '.disable_p2p // ""' "$CONFIG")
 network_fallback=$(jq -r '.network_fallback // ""' "$CONFIG")
 dry_run=$(jq -r '.dry_run // ""' $CONFIG)
+nccl_debug=$(jq -r '.nccl_debug // ""' "$CONFIG")
 outdir="${base_outdir}/${experiment_type}/${experiment}/${gpu_type}/"
 mkdir -p "$outdir"
 
@@ -48,6 +50,7 @@ topology=$(nvidia-smi topo -m)
 # NCCL env vars for interconnect control
 nccl_env=()
 nccl_comm="default"
+
 if [ "$disable_p2p" = "true" ]; then
     # disable ussing NCCL_P2P_DISABLE
     nccl_env=(NCCL_P2P_DISABLE=1)
@@ -58,6 +61,17 @@ if [ "$network_fallback" = "true" ]; then
     nccl_env=(NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1)
     nccl_comm="network fallback, (P2P, SHM disabled)"
 fi
+if [ -n "$nccl_debug" ]; then
+    # enable NCCL_DEBUG
+    nccl_env+=(NCCL_DEBUG=$nccl_debug)
+    nccl_env+=(NCCL_DEBUG_FILE="$outdir/nccl_debug_%h_%p.log")
+fi
+
+# label flag
+label_flag=""
+if [ "$label" != "" ]; then
+    label_flag="--label $label"
+fi
 
 # dry run flag
 dry_run_flag=""
@@ -66,12 +80,15 @@ if [ "$dry_run" = "true" ]; then
 fi
 
 echo "================================================================================"
-echo "experiment    : $experiment ($experiment_type)"
+echo "experiment    : $experiment ($experiment_type) $label"
 echo "model         : $model"
 echo "gpu id(s)     : $gpu_ids  ($gpu_type)"
 echo "gpu mem       : $gpu_mem"
 if [ "$nccl_comm" != "" ]; then
     echo "communication : $nccl_comm"
+fi
+if [ -n "$nccl_debug" ]; then
+    echo "nccl debug    : $nccl_debug"
 fi
 echo "tp            : $tp"
 echo "runs          : $runs"
@@ -89,7 +106,7 @@ env CUDA_VISIBLE_DEVICES=$gpu_ids "${nccl_env[@]}" \
     --tensor-parallel-size ${tp} \
     --gpu-memory-utilization ${gpu_mem} ${serve_extra}" \
     --bench-cmd "vllm bench serve \
-    --model ${model} ${bench_extra}" \
+    --model ${model} ${label_flag} ${bench_extra}" \
     --num-runs "$runs" \
     $bench_params_flag $serve_params_flag \
     --output-dir "$outdir" \
@@ -101,7 +118,7 @@ date_str=$(date +%d-%m-%Y)
 if [ ! -f "$log" ]; then
     echo "date,experiment,experiment_type,gpu_type,model,tp,outdir,config" > "$log"
 fi
-echo "${date_str},${experiment},${experiment_type},${gpu_type},${model},${tp},${outdir},${CONFIG}" >> "$log"
+echo "${date_str},${experiment},${experiment_type}:${label},${gpu_type},${model},${tp},${outdir},${CONFIG}" >> "$log"
 
 echo "========================================"
 echo "benchmark suite completed!"
